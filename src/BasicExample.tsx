3c38c134bfb8c164550fca16bdd2ca1f805b84e5
kerolos <kerolos.sss@gmail.com>
2023-03-23 21:03:28 +0200

    adding deploy script

--- src/BasicExample.tsx
+++ src/BasicExample.tsx
@@ -3,7 +3,11 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { getDemoPath, getPlaygroundPath, getTesseractPath } from "./utils/pathUtils";
+import {
+  getDemoPath,
+  getPlaygroundPath,
+  getTesseractPath,
+} from "./utils/pathUtils";
 
 type NavItemD = {
   title: string;
@@ -14,28 +18,15 @@ function MyBasicExample({ items }: { items: NavItemD[] }) {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        {items.map((item) => {
-          return <Navbar.Brand href={item.navTo}>{item.title}</Navbar.Brand>;
-        })}
-        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse> */}
+        <Nav className="me-auto">
+          {items.map((item) => {
+            return (
+              <Nav.Link key={item.navTo} href={item.navTo}>
+                {item.title}
+              </Nav.Link>
+            );
+          })}
+        </Nav>
       </Container>
     </Navbar>
   );
